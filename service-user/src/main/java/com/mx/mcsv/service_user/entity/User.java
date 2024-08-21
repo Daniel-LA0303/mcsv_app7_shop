cd317fb215672330dabedcd3cb179c7f7a391f43
Luis Alberto <87744699+Daniel-LA0303@users.noreply.github.com>
2024-08-21 10:57:36 -0600

    ...

--- service-user/src/main/java/com/mx/mcsv/service_user/entity/User.java
+++ service-user/src/main/java/com/mx/mcsv/service_user/entity/User.java
@@ -1,10 +1,11 @@
 package com.mx.mcsv.service_user.entity;
 
-import jakarta.persistence.Entity;
+/**import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Table;*/
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
