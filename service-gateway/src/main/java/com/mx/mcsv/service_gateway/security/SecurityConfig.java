59a7cbac71603ddf085aa2e9f8e50ae5903ce220
Luis Alberto <87744699+Daniel-LA0303@users.noreply.github.com>
2024-08-21 13:52:38 -0600

    ...

--- service-gateway/src/main/java/com/mx/mcsv/service_gateway/security/SecurityConfig.java
+++ service-gateway/src/main/java/com/mx/mcsv/service_gateway/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -19,4 +23,16 @@ public class SecurityConfig {
         http.csrf().disable();
         return http.build();
     }
+    
+    /*@Bean
+    public ReactiveClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration registration = ClientRegistrations
+                .fromIssuerLocation("http://localhost:8180/auth/realms/mcsv-app-shop")
+                .clientId("spring-mcsv-new")
+                .clientSecret("l1wjyO9kwzMalJjCgsKVN7C1cl92iAWM")
+                .build();
+
+        return new InMemoryReactiveClientRegistrationRepository(registration);
+    }*/
 }
+
