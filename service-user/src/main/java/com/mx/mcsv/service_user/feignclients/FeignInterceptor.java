c77c9bfc3572b6cbd0e1a66d3f19fde770258325
Luis Alberto <87744699+Daniel-LA0303@users.noreply.github.com>
2024-08-21 20:02:36 -0600

    ...

--- service-user/src/main/java/com/mx/mcsv/service_user/feignclients/FeignInterceptor.java
+++ service-user/src/main/java/com/mx/mcsv/service_user/feignclients/FeignInterceptor.java
@@ -1,5 +1,19 @@
 package com.mx.mcsv.service_user.feignclients;
 
-public class FeignInterceptor {
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Component
+public class FeignInterceptor implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate template) {
+		Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        template.header("Authorization", "Bearer " + jwt.getTokenValue());
+		
+	}
 }
